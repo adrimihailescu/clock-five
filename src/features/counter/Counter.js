756ba1a493baaa8790dc1eb6375cb6220d80bf7c
adriana <adriana.mihalescu@gmail.com>
2021-11-24 17:12:58 +0000

    added logic for start,stop and reset buttons

--- src/features/counter/Counter.js
+++ src/features/counter/Counter.js
@@ -1,21 +1,17 @@
 import React, { useState } from "react";
 import { useSelector, useDispatch } from "react-redux";
-import { selectCount } from "../session/sessionSlice";
-import { startTimer, stopTimer } from "./counterSlice";
-
-// function formatTime() {
-// 	let minutes = Math.floor(time / 60);
-// 	let time = minutes * 60;
-// 	let seconds = parseInt(time % 60, 10);
-// 	return `${minutes < 10 ? `0${minutes}` : minutes}:${
-// 		seconds < 10 ? `0${seconds}` : seconds
-// 	}`;
-// }
+import {
+	toggleTimer,
+	toggleStop,
+	toggleReset,
+	toggleSession,
+	selectCount,
+} from "./counterSlice";
 
 export function Counter() {
 	const count = useSelector(selectCount);
 	const dispatch = useDispatch();
-	const [incrementAmount, setIncrementAmount] = useState("25:00");
+	// const [session, setSession] = useState("25:00");
 
 	// const incrementValue = Number(incrementAmount) || 25;
 
@@ -23,16 +19,21 @@ export function Counter() {
 		<div>
 			<div>
 				<h2>Session</h2>
-				<input
+				{/* <input
 					type="text"
-					aria-label="Set increment amount"
 					value={count}
-					onChange={(e) => setIncrementAmount(e.target.value)}
-				/>
-
-				<button id="start_stop">Play</button>
-				<button id="start_stop">Stop Session</button>
-				<button id="reset">Reset</button>
+					// onChange={(e) => setIncrementAmount(e.target.value)}
+				/> */}
+				<p>{count}</p>
+				<button id="start_stop" onClick={() => dispatch(toggleTimer())}>
+					Play/Pause
+				</button>
+				<button id="start_stop" onClick={() => dispatch(toggleStop())}>
+					Stop Session
+				</button>
+				<button id="reset" onClick={() => dispatch(toggleReset())}>
+					Reset
+				</button>
 			</div>
 		</div>
 	);
--- src/features/counter/counterSlice.js
+++ src/features/counter/counterSlice.js
@@ -1,9 +1,9 @@
 import { createSlice } from "@reduxjs/toolkit";
 
 const initialState = {
-	value: 0,
-	isRunning: false, //for timer itself
-	isPlaying: "stopped", //for play/stop button
+	time: "25:00",
+	isSession: true,
+	isPlaying: false, //for play/stop button
 };
 
 export const counterSlice = createSlice({
@@ -11,25 +11,28 @@ export const counterSlice = createSlice({
 	initialState,
 	// The `reducers` field lets us define reducers and generate associated actions
 	reducers: {
-		startTimer: (state) => {
-			// Redux Toolkit allows us to write "mutating" logic in reducers. It
-			// doesn't actually mutate the state because it uses the Immer library,
-			// which detects changes to a "draft state" and produces a brand new
-			// immutable state based off those changes
-			state.value += 1;
+		toggleSession: (state) => {
+			state.isSession = !state.isSession;
 		},
-		stopTimer: (state) => {
-			state.value -= 1;
+		toggleTimer: (state) => {
+			state.isPlaying = !state.isPlaying;
+		},
+		toggleStop: (state) => {
+			state.isPlaying = false;
+		},
+		toggleReset: (state) => {
+			state.time = "25:00";
 		},
 		// Use the PayloadAction type to declare the contents of `action.payload`
 	},
 });
 
-export const { startTimer, stopTimer } = counterSlice.actions;
+export const { toggleTimer, toggleStop, toggleReset, toggleSession } =
+	counterSlice.actions;
 
 // The function below is called a selector and allows us to select a value from
 // the state. Selectors can also be defined inline where they're used instead of
 // in the slice file. For example: `useSelector((state: RootState) => state.counter.value)`
-export const selectCount = (state) => state.counter.value;
+export const selectCount = (state) => state.counter.time;
 
 export default counterSlice.reducer;
